797267e868756fb8dbc8260805fed5a0e4c348f6
Kherops <xzorath@gmail.com>
2025-07-21 20:03:57 +0200

    Add GitHub Actions workflow for automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    build: {
-      outDir: 'dist',
-    },
-  }
-  
-  if (command !== 'serve') {
-    config.base = '/portfoliopro/'
-  }
-  
-  return config
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 })
